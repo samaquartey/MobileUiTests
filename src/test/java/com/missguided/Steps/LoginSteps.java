782abbd3d63f041e2d2030e0be67cce02bc5551a
Sam <sam.a.quartey@outlook.com>
2020-01-16 23:18:01 +0000

    clean up code

--- src/test/java/com/missguided/Steps/LoginSteps.java
+++ src/test/java/com/missguided/Steps/LoginSteps.java
@@ -6,12 +6,14 @@ import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
 import org.junit.Assert;
+import org.picocontainer.annotations.Inject;
 
 public class LoginSteps extends LoginPage {
+
     private MyAccountPage myAccountPage;
 
-    public LoginSteps(){
-        myAccountPage = new MyAccountPage();
+    public LoginSteps(MyAccountPage myAccountPage){
+        this.myAccountPage = myAccountPage;
     }
 
     @Given("^I am on the \"([^\"]*)\" page$")
